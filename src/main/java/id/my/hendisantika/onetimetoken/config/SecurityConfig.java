55e1be2dd5bc66ca3c64620ddb782c14c09c9b86
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 09:18:05 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/onetimetoken/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.onetimetoken.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-one-time-token
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/04/25
+ * Time: 09.17
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
