ae39409ae08c6ce4e395b71ba0336db1d79ca394
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 09:16:28 +0700

    feat: Add handle in EmailGeneratedOneTimeTokenHandler🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/onetimetoken/config/EmailGeneratedOneTimeTokenHandler.java
+++ src/main/java/id/my/hendisantika/onetimetoken/config/EmailGeneratedOneTimeTokenHandler.java
@@ -2,9 +2,17 @@ package id.my.hendisantika.onetimetoken.config;
 
 import id.my.hendisantika.onetimetoken.service.CustomUserDetailsService;
 import id.my.hendisantika.onetimetoken.service.EmailService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.ott.OneTimeToken;
 import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
 import org.springframework.security.web.authentication.ott.RedirectOneTimeTokenGenerationSuccessHandler;
+import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +40,19 @@ public class EmailGeneratedOneTimeTokenHandler implements OneTimeTokenGeneration
         this.userDetailsService = userDetailsService;
     }
 
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       OneTimeToken oneTimeToken) throws IOException, ServletException {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(UrlUtils.buildFullRequestUrl(request))
+                .replacePath(request.getContextPath())
+                .replaceQuery(null)
+                .fragment(null)
+                .path("/login/ott")
+                .queryParam("token", oneTimeToken.getTokenValue());
+        String magicLink = builder.toUriString();
+        String email = getUserEmail(oneTimeToken.getUsername());
+        this.emailService.sendEmail(email, "One Time Token for the Spring Security Demo App",
+                "Use the following link to sign in into the application: " + magicLink);
+        this.redirectHandler.handle(request, response, oneTimeToken);
+    }
 }
