518e07ac98b0f16ac383b4ae5292db937dcaf435
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 09:18:28 +0700

    feat: Add authenticationManager in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/onetimetoken/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/onetimetoken/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package id.my.hendisantika.onetimetoken.config;
 
 import id.my.hendisantika.onetimetoken.service.CustomOneTimeTokenService;
+import id.my.hendisantika.onetimetoken.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -50,4 +55,14 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(CustomUserDetailsService userDetailsService,
+                                                       PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
 }
