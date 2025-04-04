aee8b7cb425760bd7be802de2e7d47b5f00c4444
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 09:05:38 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/onetimetoken/SpringBootOneTimeTokenApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.onetimetoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOneTimeTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOneTimeTokenApplication.class, args);
+    }
+
+}
