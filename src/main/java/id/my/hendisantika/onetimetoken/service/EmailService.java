509b1b73253db1bceda151fff5aa3c6595b41bc6
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 09:15:31 +0700

    feat: Add sendEmail in EmailService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/onetimetoken/service/EmailService.java
+++ src/main/java/id/my/hendisantika/onetimetoken/service/EmailService.java
@@ -2,6 +2,8 @@ package id.my.hendisantika.onetimetoken.service;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +18,22 @@ import org.springframework.stereotype.Component;
  * Time: 09.14
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    public void sendEmail(String to, String subject, String body) {
+        log.info("Sending email to: {} with subject: {} and body: {}", to, subject, body);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        log.info("Mail sent");
+    }
 }
