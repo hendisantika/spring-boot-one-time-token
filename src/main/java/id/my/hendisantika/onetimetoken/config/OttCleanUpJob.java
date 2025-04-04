0ffeae3b92be259fe68b53b99b6b986c6ca3b653
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 09:17:47 +0700

    feat: Add cleanUpOldRecords in OttCleanUpJob🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/onetimetoken/config/OttCleanUpJob.java
+++ src/main/java/id/my/hendisantika/onetimetoken/config/OttCleanUpJob.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.onetimetoken.config;
 
 import id.my.hendisantika.onetimetoken.repository.OttTokenRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,12 +20,18 @@ import org.springframework.stereotype.Component;
  * Time: 09.16
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class OttCleanUpJob {
 
     private final OttTokenRepository tokenRepository;
 
-    public OttCleanUpJob(OttTokenRepository tokenRepository) {
-        this.tokenRepository = tokenRepository;
+    @Transactional
+    @Scheduled(fixedRate = 3600000) // 3600000 ms = 1 hour
+    public void cleanUpOldRecords() {
+        log.info("Deleting expired tokens");
+        int deleted = tokenRepository.deleteExpiredTokens(Instant.now());
+        log.info("Deleted {} expired tokens", deleted);
     }
 }
